cfd1152d8b8b05d330684e52561310cf8eef6c96
Sumit1arora <94929971+Sumit1arora@users.noreply.github.com>
2024-12-13 19:55:03 +0530

    Refactor

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
 from backend.app.auth.routes import router as auth_router
 from backend.app.listings.routes import router as listings_router
-from backend.app.recommendations.routes import router as recommendations_router
 from backend.app.database import Base, engine
 
 Base.metadata.create_all(bind=engine)
@@ -10,7 +9,6 @@ app = FastAPI()
 
 app.include_router(auth_router, prefix="/auth", tags=["Auth"])
 app.include_router(listings_router, prefix="/listings", tags=["Listings"])
-app.include_router(recommendations_router, prefix="/recommendations", tags=["Recommendations"])
 
 @app.get("/")
 def root():
