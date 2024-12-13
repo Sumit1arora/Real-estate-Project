c998200027d6754b54d51fdf267a47f0500f9cf1
Sumit1arora <94929971+Sumit1arora@users.noreply.github.com>
2024-12-13 20:24:40 +0530

    Refactor

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from backend.app.auth.routes import router as auth_router
 from backend.app.listings.routes import router as listings_router
 from backend.app.database import Base, engine
@@ -6,6 +7,16 @@ from backend.app.database import Base, engine
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+origins = [*]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth_router, prefix="/auth", tags=["Auth"])
 app.include_router(listings_router, prefix="/listings", tags=["Listings"])
